5f8775fca9a98f62f19c5acce285d0046d368f18
Wolfffff <22243650+Wolfffff@users.noreply.github.com>
2024-04-10 16:10:47 -0400

    First pre-commit

--- data_processing/plot_network_no_filtering.py
+++ data_processing/plot_network_no_filtering.py
@@ -1,13 +1,24 @@
 import sys
-import networkx as nx
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import pandas as pd
 
 df = pd.read_table(sys.argv[1])
-df_onetype = df[df['Interaction Name'] == sys.argv[3]]
-counts = pd.DataFrame({'count' : df_onetype.groupby( [ "Origin interactor", "Destination interactor"] ).size()}).reset_index()
+df_onetype = df[df["Interaction Name"] == sys.argv[3]]
+counts = pd.DataFrame(
+    {
+        "count": df_onetype.groupby(
+            ["Origin interactor", "Destination interactor"]
+        ).size()
+    }
+).reset_index()
 counts["log_count"] = np.log10(counts["count"])
-G = nx.from_pandas_edgelist(counts, source='Origin interactor', target='Destination interactor', edge_attr=('count', 'log_count'))
+G = nx.from_pandas_edgelist(
+    counts,
+    source="Origin interactor",
+    target="Destination interactor",
+    edge_attr=("count", "log_count"),
+)
 nx.write_edgelist(G, sys.argv[2])
