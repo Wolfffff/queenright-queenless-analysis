ab8f11b535bfe9172302a931f3806d9351b15b9d
Wolfffff <22243650+Wolfffff@users.noreply.github.com>
2024-07-04 14:14:49 -0400

    Fig 2

--- scripts/manuscript/figure_2.R
+++ scripts/manuscript/figure_2.R
@@ -3,7 +3,16 @@ library(lme4)
 library(tidyverse)
 library(stringr)
 library(wesanderson)
-library(patchwork)
+library(cowplot)
+
+Q_QRW_INT_QLW <- list(
+  Q = "#893F71",
+  QRW = "#AC6F82",
+  INF = "#CC5500",
+  QLW = "#FFAE00"
+)
+
+COLONY_COLORS <- wes_palette("Cavalcanti1")
 
 # Read the data
 bds <- read_csv("data/BigDataSheet.csv")
@@ -64,35 +73,35 @@ consistent_theme <- theme_minimal() +
     theme(
         plot.title = element_text(hjust = 0.5),
         legend.position = "None",
-        text = element_text(size = 7),
+        text = element_text(size = 9),
         panel.grid.major.x = element_line(color = "grey", linetype = "dashed"),
         panel.grid.minor.x = element_line(color = "grey", linetype = "dotted"),
         panel.grid.major.y = element_blank(),
         panel.grid.minor.y = element_blank(),
         axis.line.x = element_line(color = "black", size = 0.5),
         axis.line.y = element_line(color = "black", size = 0.5),
-        strip.text = element_text(size = 7, face = "bold"),
+        strip.text = element_text(size = 9, face = "bold"),
         axis.text.y.right = element_blank(),
         axis.ticks.y.right = element_blank(),
         aspect.ratio = 1,
         axis.text.x = element_text(size = 8),
-        axis.text.y = element_text(size = 8),
-        plot.margin = unit(c(0.1,0.1,0.1,0.1), "cm")
+        axis.text.y = element_text(size = 9),
+        plot.margin = unit(c(0.1, 0.1, 0.1, 0.1), "cm")
     )
 # Plot Degree
 plot_degree <- ggplot(bds_mean_of_means, aes(x = QR_Queen_Condition, y = Degree)) +
     geom_line(aes(group = Trial), color = "darkgray", linewidth = 0.2) +
     geom_point(aes(color = Trial), size = 3) +
-    scale_color_manual(values = wes_palette("Cavalcanti1")) +
+    scale_color_manual(values = COLONY_COLORS) +
     xlab("") +
-    ylab("Standardized Number of Interactions per Hour") +
+    ylab("Std. Number of Int. per Hour") +
     consistent_theme
 
 # Plot Initiation.Freq
 plot_init_freq <- ggplot(bds_mean_of_means, aes(x = QR_Queen_Condition, y = Initiation.Freq)) +
     geom_line(aes(group = Trial), color = "darkgray", linewidth = 0.2) +
     geom_point(aes(color = Trial), size = 3) +
-    scale_color_manual(values = wes_palette("Cavalcanti1")) +
+    scale_color_manual(values = COLONY_COLORS) +
     xlab("") +
     ylab("Initiation Frequency") +
     consistent_theme
@@ -101,22 +110,22 @@ plot_init_freq <- ggplot(bds_mean_of_means, aes(x = QR_Queen_Condition, y = Init
 plot_n90_day4 <- ggplot(bds_mean_of_means, aes(x = QR_Queen_Condition, y = N90.Day4)) +
     geom_line(aes(group = Trial), color = "darkgray", linewidth = 0.2) +
     geom_point(aes(color = Trial), size = 3) +
-    scale_color_manual(values = wes_palette("Cavalcanti1")) +
+    scale_color_manual(values = COLONY_COLORS) +
     xlab("") +
     ylab("N90 (Dispersion)") +
     consistent_theme
 
 # Rug
 bds$Alpha <- ifelse(bds$Queen, .5, 0.005)
-bds$PointSize <- ifelse(bds$Queen, .003, .001)
+bds$PointSize <- ifelse(bds$Queen, .05, .005)
 
 degree_over_time <- ggplot(bds, aes(x = as.integer(Hour), y = Degree / 20, group = ID)) +
     geom_jitter(aes(color = QR_Queen_Condition, alpha = Alpha, size = PointSize)) +
     geom_smooth(aes(group = QR_Queen_Condition, color = QR_Queen_Condition), se = FALSE) +
-    scale_size(range = c(.001, .2)) +
+    scale_size(range = c(.001, .5)) +
     scale_color_manual(
         labels = c("Queenright Worker", "Queenless Worker", "Queen"),
-        values = c("#629CC0", "#CEB175", "#E54E21"),
+        values = c(Q_QRW_INT_QLW$QRW, Q_QRW_INT_QLW$QLW, Q_QRW_INT_QLW$Q),
         guide = guide_legend(direction = "horizontal")
     ) +
     scale_x_continuous(breaks = c(0, seq(24, 96, by = 24)), limits = c(0, NA), expand = c(0, 0)) + # Expand limits to include 0
@@ -125,36 +134,37 @@ degree_over_time <- ggplot(bds, aes(x = as.integer(Hour), y = Degree / 20, group
     ylab("Time Spent Interacting per Hour (s)") +
     theme_minimal() +
     theme(
+        text = element_text(size = 9),
         plot.title = element_text(hjust = 0.5),
         legend.position = c(1, 1.02), # Move legend to top right
         legend.justification = c(1, 1), # Align legend to top right
-        legend.background = element_rect(fill = alpha("white", 1)),
-        theme(legend.key = element_rect(fill = "white")),
-        legend.title = element_text(size = 7, face = "bold"),
-        legend.text = element_text(size = 7),
+        legend.background = element_rect(fill = alpha("white", 1), color = NA), # Add white background to legend
+        legend.title = element_text(size = 9, face = "bold"),
+        legend.text = element_text(size = 9),
         panel.grid.major.x = element_line(color = "grey", linetype = "dashed"), # Keep vertical grid lines
         panel.grid.minor.x = element_line(color = "grey", linetype = "dotted"), # Keep vertical grid lines
         panel.grid.major.y = element_blank(), # Remove horizontal grid lines
         panel.grid.minor.y = element_blank(), # Remove horizontal grid lines
         axis.line.x = element_line(color = "black", size = 0.5), # Add x-axis line
         axis.line.y = element_line(color = "black", size = 0.5), # Add y-axis line
-        strip.text = element_text(size = 7, face = "bold"), # Make facet plot titles larger and bold
+        strip.text = element_text(size = 9, face = "bold"), # Make facet plot titles larger and bold
         panel.grid = element_blank(),
         panel.border = element_blank(),
-        axis.text.x = element_text(size = 8),
-        axis.text.y = element_text(size = 8),
-        plot.margin = unit(c(0.1,0.1,0.1,0.1), "cm") # Add margin for alignment
+        axis.text.x = element_text(size = 9),
+        axis.text.y = element_text(size = 9),
+        plot.margin = unit(c(0.1, 0.1, 0.1, 0.1), "cm") # Add margin for alignment
     ) +
     guides(alpha = "none", size = "none")
 
 # Align the top left plot and the bottom plot
-plots <- align_plots(plot_degree, degree_over_time, align = 'v', axis = 'l')
+plots <- align_plots(plot_degree, degree_over_time, align = "v", axis = "l")
 
 # Create the top row with the aligned top left plot and the other two top row plots
-top_row <- plot_grid(plots[[1]], plot_init_freq, plot_n90_day4, nrow = 1, align = 'hv', axis = 'tblr')
+top_row <- plot_grid(plots[[1]], plot_init_freq, plot_n90_day4, nrow = 1, align = "hv", axis = "tblr")
 
 # Create the final plot with the top row and the bottom plot
-final_plot <- plot_grid(top_row, plots[[2]], ncol = 1, rel_heights = c(1, 1))
+final_plot <- plot_grid(plots[[2]],top_row, ncol = 1, rel_heights = c(1, 1))
 
 # Save the combined plot
-ggsave("figures/manuscript/figure_1_combined.png", plot = final_plot, width = 8.5, height = 6)
+# Adjust the plot saving command to include margins
+ggsave("figures/manuscript/figure_2_combined.jpeg", plot = final_plot + theme(plot.margin = margin(1, 1, 1, 1, "cm")), width = 8.5, height = 6)
